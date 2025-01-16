7b0f906a0f935411fbaab6347357e164c0683ccf
Apolo Barua Apurbo <apolo.itnet@gmail.com>
2025-01-17 01:45:33 +0600

    portfolio-project-home-section-stripe-lines-update

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 
 window.addEventListener('scroll', function () {
   const navbar = document.querySelector('.navbar');
+  const navLogo = document.querySelector('.nav-logo');
+  const navMenu = document.querySelector('.nav-menu');
   if (window.scrollY > 50) { // Adjust scroll value as needed
     navbar.classList.add('scrolled');
+    navLogo.classList.add('scrolled');
+    navMenu.classList.add('scrolled');
   } else {
     navbar.classList.remove('scrolled');
+    navLogo.classList.remove('scrolled');
+    navMenu.classList.remove('scrolled');
   }
 });
